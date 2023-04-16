88fafe9a7681033880530f3ed4cd0a3a68be22f9
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-16 10:47:27 +0530

    airbnb

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+
+const Search = ()=>{
+   return (
+    <div 
+     className="
+      border-[1px]
+      w-full
+      md:w-auto
+      py-2
+      rounded-full
+      shadow-sm
+      hover:shadow-md
+      transition
+      cursor-pointer
+
+     ">
+        <div
+           className=" flex flex-row items-center justify-between "
+        >
+          <div className="text-sm font-semibold px-6">
+          anywhere
+          </div>
+          <div className="hidden sm:block test-sm font-semibold px-3 border-x-[1px] flex-1 text-center">
+            anyweek
+          </div>
+        </div>
+    </div>
+   );
+}
+
+export default Search;
